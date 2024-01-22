e97b84f6224636443c5119383dae0c4bb8fe6e9a
Gustavo Holanda de Sousa <106280222+GustavoHolanda16@users.noreply.github.com>
2024-01-22 15:24:30 -0300

    fim2

--- filme.js
+++ filme.js
@@ -1,7 +1,7 @@
-// Função para carregar um filme padrão na inicialização da página
+
 function carregarFilmePadrao() {
     const apiKey = '790af7bc';
-    const filmePadrao = 'The Matrix'; // Título do filme padrão
+    const filmePadrao = 'The Matrix'; 
 
     const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&t=${filmePadrao}`;
 
@@ -22,7 +22,7 @@ function carregarFilmePadrao() {
         .catch(error => console.error(`Erro na solicitação: ${error.message}`));
 }
 
-// Função para exibir detalhes do filme
+
 function exibirDetalhesDoFilme(data) {
     document.getElementById('poster').src = data.Poster;
     document.getElementById('titulo').textContent = data.Title;
@@ -32,9 +32,9 @@ function exibirDetalhesDoFilme(data) {
     document.getElementById('lancamento').textContent = data.Year;
 }
 
-// Função para exibir mensagem de filme não encontrado
+
 function exibirFilmeNaoEncontrado() {
-    // Limpar os campos
+
     document.getElementById('poster').src = '';
     document.getElementById('titulo').textContent = '';
     document.getElementById('sinopse').textContent = '';
@@ -42,12 +42,12 @@ function exibirFilmeNaoEncontrado() {
     document.getElementById('duracao').textContent = '';
     document.getElementById('lancamento').textContent = '';
 
-    // Exibir a mensagem de filme não encontrado
+
     document.getElementById('poster').src = 'https://github.com/GustavoHolanda16/filmes/blob/main/img/image-removebg-preview.png?raw=true';
     document.getElementById('titulo').textContent = 'Filme não encontrado!';
 }
 
-// Função para buscar um filme
+
 function buscar() {
     const apiKey = '790af7bc';
     const movieTitle = document.getElementById('movieTitle').value;
@@ -76,5 +76,5 @@ function buscar() {
         .catch(error => console.error(`Erro na solicitação: ${error.message}`));
 }
 
-// Carregar um filme padrão na inicialização da página
+
 document.addEventListener('DOMContentLoaded', carregarFilmePadrao);
